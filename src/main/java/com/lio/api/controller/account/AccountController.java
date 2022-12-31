dfeadbfc55b576972496c27b3f361f9f6e3f578f
Myo Min Htoo <myominhtoo2003@gmail.com>
2023-01-01 00:40:20 +0630

    added routes in routes.yml

---
+++ src/main/java/com/lio/api/controller/account/AccountController.java
@@ -0,0 +1,12 @@
+package com.lio.api.controller.account;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping( value = "${api.current.version}" )
+public class AccountController{
+    
+
+}
